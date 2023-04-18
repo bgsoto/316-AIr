e867e18e69b247c092c6d88ea2972fa5e4f547f4
bgsoto <115710071+bgsoto@users.noreply.github.com>
2023-04-18 11:50:15 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerNavMesh.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+
+
+public class PlayerNavMesh : MonoBehaviour
+{
+
+    [SerializeField] private Transform movePositionTransform;
+
+
+    private NavMeshAgent agent1;
+    private void Awake()
+    {
+       
+       agent1 =  GetComponent <NavMeshAgent>();
+
+    }
+
+    private void Update()
+    {
+        agent1.destination = movePositionTransform.position;
+    }
+
+}
